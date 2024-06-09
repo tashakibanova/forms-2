c53688c9b4c7912206396dbb895fc8dbfb775ed1
tashakibanova <ov.nattie@gmail.com>
2024-06-09 18:12:28 +0300

    upd test JSDOM

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,4 +1,4 @@
-import ShowPopover from '../engine/showPopover';
+/*import ShowPopover from '../engine/showPopover';
 import app from '../app';
 
 jest.mock('../engine/showPopover');
@@ -25,4 +25,50 @@ test('app() should call method init', () => {
   const mockInit = showPopoverInstance.init;
 
   expect(mockInit).toHaveBeenCalledTimes(1);
+});*/
+
+import { JSDOM } from 'jsdom';
+import ShowPopover from '../engine/showPopover';
+import app from '../app';
+
+jest.mock('../engine/showPopover');
+
+describe('ShowPopover DOM tests with JSDOM', () => {
+  let dom;
+  let container;
+
+  beforeEach(() => {
+    // Создаем новое экземпляр JSDOM
+    dom = new JSDOM('<!DOCTYPE html><html><body><div id="app"></div></body></html>', {
+      runScripts: 'dangerously',
+      resources: 'usable'
+    });
+
+    // Получаем контейнер для нашего теста
+    container = dom.window.document.getElementById('app');
+
+    ShowPopover.mockClear();
+  });
+
+  test('new ShowPopover wont be created automatically', () => {
+    expect(ShowPopover).not.toHaveBeenCalled();
+  });
+
+  test('app() should create new ShowPopover', () => {
+    dom.window.app = app;
+    dom.window.app();
+    expect(ShowPopover).toHaveBeenCalledTimes(1);
+  });
+
+  test('app() should call method init', () => {
+    dom.window.app = app;
+    expect(ShowPopover).not.toHaveBeenCalled();
+    dom.window.app();
+    expect(ShowPopover).toHaveBeenCalledTimes(1);
+
+    const showPopoverInstance = ShowPopover.mock.instances[0];
+    const mockInit = showPopoverInstance.init;
+
+    expect(mockInit).toHaveBeenCalledTimes(1);
+  });
 });
